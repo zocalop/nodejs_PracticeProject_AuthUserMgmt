3efd4d7858315ac7871f7a64db5e66bb3fdec60e
zocalop <jcb2861@gmail.com>
2026-07-11 13:46:08 -0300

    Updated index.js in main

--- index.js
+++ index.js
@@ -111,4 +111,4 @@ const PORT =5000;
 
 app.use("/friends", routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT,()=>console.log("Server is running at port: " + PORT));
